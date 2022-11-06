1704d7c8677620a7bb64726082ee1cbe0e00d9d5
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-11-05 22:11:43 -0400

    Added 'meetings' to calendar

--- src/App.js
+++ src/App.js
@@ -2,63 +2,109 @@ import { useState } from "react";
 import dayjs from "dayjs";
 import generateDate, { months } from "./util/Calendar";
 import cn from "./util/cn";
-import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr';
-
+import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
 
 function App() {
-
   const days = ["S", "M", "T", "W", "T", "F", "S"];
   const currentDate = dayjs();
   const [today, setToday] = useState(currentDate);
-  const [selectedDate, setSelectedDate] = useState(currentDate);
+  const [selectedDate, setSelectedDate] = useState({getDate: currentDate, today: true});
+  const [meetings, setMeetings] = useState([]);
 
   return (
-  <div className="flex w-1/2 mx-auto divide-x-2 gap-10 h-screen items-center">
-    <div className="w-96 h-96 ">
-      <div className="flex justify-between">
+    <div className="flex w-1/2 mx-auto divide-x-2 gap-10 h-screen items-center">
+      <div className="w-96 h-96 ">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="font-semibold">
+              {months[today.month()]}, {today.year()}
+            </h1>
+          </div>
 
-        <div>
-          <h1 className="font-semibold">{months[today.month()]}, {today.year()}</h1>
+          <div className="flex items-center gap-5">
+            <GrCaretPrevious
+              className="cursor-pointer"
+              onClick={() => {
+                setToday(today.month(today.month() - 1));
+              }}
+            />
+
+            <h1
+              className="cursor-pointer"
+              onClick={() => {
+                setToday(currentDate);
+                setSelectedDate({getDate: currentDate, today: true});
+              }}
+            >
+              Today
+            </h1>
+
+            <GrCaretNext
+              className="cursor-pointer"
+              onClick={() => {
+                setToday(today.month(today.month() + 1));
+              }}
+            />
+          </div>
         </div>
-
-        <div className="flex items-center gap-5">
-          <GrCaretPrevious className="cursor-pointer" onClick={() => {setToday(today.month(today.month() - 1))}}/>
-
-          <h1 className="cursor-pointer" onClick={() => {setToday(currentDate); setSelectedDate(currentDate)}}>Today</h1>
-          
-          <GrCaretNext className="cursor-pointer" onClick={() => {setToday(today.month(today.month() + 1))}}/>
+        <div className="w-full grid grid-cols-7">
+          {days.map((day, index) => {
+            // S M T...F S
+            return (
+              <h1
+                key={index}
+                className="h-10 grid place-content-center text-sm text-gray-600"
+              >
+                {day}
+              </h1>
+            );
+          })}
         </div>
+        <div className="w-full grid grid-cols-7">
+          {generateDate(today.month(), today.year()).map(
+            ({ date, currentMonth, isToday, meetings }, index) => {
+              //today.month(), today.year()
 
+              return (
+                <div
+                  key={index}
+                  className="h-14 border-t grid place-content-center text-sm"
+                >
+                  <h1
+                    className={cn(
+                      currentMonth ? "" : "text-gray-400",
+                      isToday ? "bg-purple-700 text-white" : "",
+                      "h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all cursor-pointer"
+                    )}
+                    onClick={() => {
+                      setSelectedDate({getDate: date, today: isToday});
+                      setMeetings([...meetings]);
+                    }}
+                  >
+                    {date.date()}
+                  </h1>
+                </div>
+              );
+            }
+          )}
+        </div>
       </div>
-      <div className="w-full grid grid-cols-7">
-      {
-        days.map((day, index) => {
-          return <h1 key={index} className="h-10 grid place-content-center text-sm text-gray-600">{day}</h1>
-        })
-      }
-      </div>
-      <div className="w-full grid grid-cols-7">
-      {generateDate(today.month(), today.year()).map(({date, currentMonth, isToday}, index) => {//today.month(), today.year()
-
+      <div className="h-96 w-96 pl-5">
+        <h1 className="font-semibold">
+          Schedule for {selectedDate.getDate.toDate().toDateString()}
+        </h1>
+        {meetings.map((obj) => {//display meetings, if any]] 
           return (
-              <div key={index} className="h-14 border-t grid place-content-center text-sm">
-                <h1 
-                className=
-                {cn(currentMonth ? "" : "text-gray-400", 
-                    isToday ? "bg-purple-700 text-white" : "",
-                    "h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all cursor-pointer"
-                )} onClick={() => {setSelectedDate(date)}}>{date.date()}</h1>
+            <div class="flex justify-left mb-2">
+              <div class={cn(selectedDate.today ? "bg-purple-600 text-white" : "bg-gray-400", "block p-4 rounded-lg shadow-lg w-80")}>
+                <h2 class=" text-xl leading-tight font-medium mb-2">{obj.title}</h2>
+                <p class=" text-base ">{obj.time}</p>
               </div>
+            </div>
           );
-
-      })}
+        })}
       </div>
     </div>
-    <div className="h-96 w-96 pl-5">
-      <h1 className="font-semibold">Schedule for {selectedDate.toDate().toDateString()}</h1>
-      <p>Lorem Ipsum whatever...</p>
-    </div>
-  </div>
   );
 }
 
--- src/util/Calendar.js
+++ src/util/Calendar.js
@@ -9,15 +9,29 @@ const generateDate = (month = dayjs().month(), year = dayjs().year()) => {// Val
 
     //generate prefix dates
     for(let i = 0; i < startOfMonth.day(); i++)
-        dates.push({date: startOfMonth.day(i), currentMonth: false});
+        dates.push({date: startOfMonth.day(i), currentMonth: false, 
+        meetings: [{
+            title: 'Call Alex',
+            time: startOfMonth.day(i).hour(13).minute(30).format('H:mm')
+        }]});
 
     //generate current dates
     for(let i = startOfMonth.date(); i <= endOfMonth.date(); i++)
-        dates.push({date: startOfMonth.date(i), currentMonth: true, isToday: startOfMonth.date(i).isSame(dayjs().toDate().toDateString(), 'day')});
+        dates.push({date: startOfMonth.date(i), currentMonth: true, isToday: startOfMonth.date(i).isSame(dayjs().toDate().toDateString(), 'day'), 
+        meetings: [{
+            title: 'Some other important meeting',
+            time: startOfMonth.day(i).hour(5).minute(30).format('HH:mm')
+        },
+        
+        {
+            title: 'Call Alex',
+            time: startOfMonth.day(i).hour(13).minute(30).format('H:mm')
+        }
+        ]});
 
     //generate suffix dates
     for(let i = endOfMonth.day() + 1; i < 7; i++)
-        dates.push({date: endOfMonth.day(i), currentMonth: false});
+        dates.push({date: endOfMonth.day(i), currentMonth: false, meetings: []});
 
     return dates;
 }
